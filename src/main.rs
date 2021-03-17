c49026d8dd870f98f72ae94ad0d03b55c79ecd73
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-03-18 01:52:54 +1300

    this is all temporary so I'm not gonna bother tracking the changes here lol

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,55 @@ use std::collections::HashMap;
 
 use druid::{
     widget::{Checkbox, Flex, Label, Slider, TextBox},
-    AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc, 
+    AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc,
 };
 
 mod graph_data;
 mod graph_widget;
+mod port_widget;
 mod vertex_data;
 mod vertex_widget;
 
 use crate::graph_data::GraphData;
 use crate::graph_widget::GraphWidget;
+use crate::port_widget::PortWidget;
 use crate::vertex_data::{BoolInputLens, FloatInputLens, Packet, StringInputLens, VertexData};
 use crate::vertex_widget::VertexWidget;
 
 fn main() -> Result<(), PlatformError> {
-    let main_window = WindowDesc::new(ui_builder);
+    let main_window = WindowDesc::new(ui_builder());
     let mut data = GraphData::new();
     let mut inputs_1 = HashMap::new();
     inputs_1.insert("1", Packet::Float(5.));
     let mut outputs_1 = HashMap::new();
     outputs_1.insert("2", Packet::Float(5.));
+    let mut id = data.get_vertices().len();
     data.get_vertices_mut().push_back(VertexData::new(
         inputs_1,
         outputs_1,
+        id,
         placeholder_generator_thing,
     ));
     let mut inputs_2 = HashMap::new();
     inputs_2.insert("1", Packet::Bool(true));
     inputs_2.insert("2", Packet::String("it work".to_string()));
     let mut outputs_2 = HashMap::new();
+    id = data.get_vertices().len();
     data.get_vertices_mut().push_back(VertexData::new(
         inputs_2.clone(),
         outputs_2.clone(),
+        id,
         placeholder_generator_thing_2,
     ));
+    id = data.get_vertices().len();
     data.get_vertices_mut().push_back(VertexData::new(
         inputs_2,
         outputs_2,
+        id,
         placeholder_generator_thing_2,
     ));
     AppLauncher::with_window(main_window)
-        .use_simple_logger()
+        .log_to_console()
         .launch(data)
 }
 
@@ -51,7 +59,7 @@ fn ui_builder() -> impl Widget<GraphData> {
     // .debug_paint_layout()
 }
 
-fn placeholder_generator_thing() -> Box<dyn Widget<VertexData>> {
+fn placeholder_generator_thing(data: &VertexData) -> Box<dyn Widget<VertexData>> {
     Box::new(VertexWidget::new(
         Flex::column()
             .with_child(Label::new("Node Title 1").center())
@@ -64,7 +72,7 @@ fn placeholder_generator_thing() -> Box<dyn Widget<VertexData>> {
     ))
 }
 
-fn placeholder_generator_thing_2() -> Box<dyn Widget<VertexData>> {
+fn placeholder_generator_thing_2(data: &VertexData) -> Box<dyn Widget<VertexData>> {
     Box::new(VertexWidget::new(
         Flex::column()
             .with_child(Label::new("Node Title 2").center())
@@ -77,6 +85,7 @@ fn placeholder_generator_thing_2() -> Box<dyn Widget<VertexData>> {
             )
             .with_spacer(5.)
             .with_child(Checkbox::new("1").lens(BoolInputLens("1")).align_left())
-            .with_spacer(5.),
+            .with_spacer(5.)
+            .with_child(PortWidget::new(data.id(), "hey")),
     ))
 }
