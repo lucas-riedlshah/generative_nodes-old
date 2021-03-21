eddd4b7606c375753b7cf16f2d02f5d8600a2026
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-03-22 03:29:42 +1300

    Playing around with how a basic node/vertex might be constructed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use druid::{
-    widget::{Checkbox, Flex, Label, Slider, TextBox},
+    widget::{Checkbox, CrossAxisAlignment, Flex, Label, Slider, TextBox},
     AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc,
 };
 
@@ -12,7 +12,7 @@ mod vertex_data;
 mod vertex_widget;
 
 use crate::graph_data::GraphData;
-use crate::graph_widget::GraphWidget;
+use crate::graph_widget::{Direction, GraphWidget, Port};
 use crate::port_widget::PortWidget;
 use crate::vertex_data::{BoolInputLens, FloatInputLens, Packet, StringInputLens, VertexData};
 use crate::vertex_widget::VertexWidget;
@@ -21,9 +21,9 @@ fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(ui_builder());
     let mut data = GraphData::new();
     let mut inputs_1 = HashMap::new();
-    inputs_1.insert("1", Packet::Float(5.));
+    inputs_1.insert("float1", Packet::Float(5.));
     let mut outputs_1 = HashMap::new();
-    outputs_1.insert("2", Packet::Float(5.));
+    outputs_1.insert("float2", Packet::Float(5.));
     let mut id = data.get_vertices().len();
     data.get_vertices_mut().push_back(VertexData::new(
         inputs_1,
@@ -32,9 +32,10 @@ fn main() -> Result<(), PlatformError> {
         placeholder_generator_thing,
     ));
     let mut inputs_2 = HashMap::new();
-    inputs_2.insert("1", Packet::Bool(true));
-    inputs_2.insert("2", Packet::String("it work".to_string()));
+    inputs_2.insert("bool", Packet::Bool(true));
+    inputs_2.insert("string", Packet::String("it work".to_string()));
     let mut outputs_2 = HashMap::new();
+    outputs_2.insert("bool_out", Packet::Bool(false));
     id = data.get_vertices().len();
     data.get_vertices_mut().push_back(VertexData::new(
         inputs_2.clone(),
@@ -67,7 +68,7 @@ fn placeholder_generator_thing(data: &VertexData) -> Box<dyn Widget<VertexData>>
                 Slider::new()
                     .with_range(-10., 10.)
                     .expand_width()
-                    .lens(FloatInputLens("1")),
+                    .lens(FloatInputLens("float1")),
             ),
     ))
 }
@@ -75,17 +76,48 @@ fn placeholder_generator_thing(data: &VertexData) -> Box<dyn Widget<VertexData>>
 fn placeholder_generator_thing_2(data: &VertexData) -> Box<dyn Widget<VertexData>> {
     Box::new(VertexWidget::new(
         Flex::column()
-            .with_child(Label::new("Node Title 2").center())
+            .with_child(Label::new("Node Title 2"))
             .with_spacer(5.)
             .with_child(
-                TextBox::new()
-                    .lens(StringInputLens("2"))
-                    .expand_width()
-                    .align_left(),
+                // Inputs
+                Flex::column()
+                    .cross_axis_alignment(CrossAxisAlignment::Start)
+                    .with_child(
+                        Flex::row()
+                            .with_child(PortWidget::new(Port::new(
+                                data.id(),
+                                "string",
+                                Direction::Input,
+                            )))
+                            .with_spacer(5.)
+                            .with_child(TextBox::new().lens(StringInputLens("string"))),
+                    )
+                    .with_spacer(5.)
+                    .with_child(
+                        Flex::row()
+                            .with_child(PortWidget::new(Port::new(
+                                data.id(),
+                                "bool",
+                                Direction::Input,
+                            )))
+                            .with_spacer(5.)
+                            .with_child(Checkbox::new("bool").lens(BoolInputLens("bool"))),
+                    )
+                    .expand_width(),
             )
             .with_spacer(5.)
-            .with_child(Checkbox::new("1").lens(BoolInputLens("1")).align_left())
-            .with_spacer(5.)
-            .with_child(PortWidget::new(data.id(), "hey")),
+            .with_child(
+                // Outputs
+                Flex::column()
+                    .cross_axis_alignment(CrossAxisAlignment::End)
+                    .with_child(PortWidget::new(Port::new(
+                        data.id(),
+                        "bool_out",
+                        Direction::Output,
+                    )))
+                    .expand_width(),
+            )
+            .fix_width(150.)
+            .padding(5.),
     ))
 }
