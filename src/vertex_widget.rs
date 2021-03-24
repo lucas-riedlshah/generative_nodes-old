d80721c19f25fec550c72873ffc5f99750a648c7
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-03-25 00:28:42 +1300

    comma

--- src/vertex_widget.rs
+++ src/vertex_widget.rs
@@ -1,7 +1,7 @@
 use druid::{
-    BoxConstraints, Color, Command, Data, Env, Event, EventCtx, LayoutCtx,
-    LifeCycle, LifeCycleCtx, PaintCtx, Point, RenderContext, Selector, Size, Target, UpdateCtx,
-    Widget, WidgetExt, WidgetPod,
+    BoxConstraints, Color, Command, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx,
+    PaintCtx, Point, RenderContext, Selector, Size, Target, UpdateCtx, Widget, WidgetExt,
+    WidgetPod,
 };
 
 // Need to investigate ways to totally remove this widget as I don't think it's actually needed.
@@ -9,15 +9,12 @@ use druid::{
 
 pub struct VertexWidget<T> {
     inner: WidgetPod<T, Box<dyn Widget<T>>>,
-    // Might be worth moving this out to the app state.
-    is_selected: bool,
 }
 
 impl<T: Data> VertexWidget<T> {
     pub fn new<W: Widget<T> + 'static>(inner: W) -> Self {
         VertexWidget {
             inner: WidgetPod::new(inner.boxed()),
-            is_selected: false,
         }
     }
 }
@@ -26,22 +23,11 @@ impl<T: Data> Widget<T> for VertexWidget<T> {
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
         self.inner.event(ctx, event, data, env);
         match event {
-            Event::Command(command) => {
-                if let Some(value) = command.get(Selector::<bool>::new("update_selected")) {
-                    self.is_selected = *value;
-                    ctx.request_layout();
-                }
-            }
             Event::MouseDown(mouse) => {
                 if !self.inner.has_active() {
                     ctx.request_focus();
                     ctx.set_active(true);
                 }
-                ctx.submit_notification(Command::new(
-                    Selector::new("vertex_clicked"),
-                    mouse.clone(),
-                    Target::Auto,
-                ));
                 ctx.set_handled();
             }
             Event::MouseUp(_mouse) => {
@@ -79,11 +65,7 @@ impl<T: Data> Widget<T> for VertexWidget<T> {
         let clip_rect = ctx.size().to_rect().to_rounded_rect(10.);
         ctx.fill(clip_rect, &Color::rgba8(50, 50, 50, 230));
 
-        if self.is_selected {
-            ctx.stroke(clip_rect, &Color::rgb8(200, 50, 150), 1.);
-        } else {
-            ctx.stroke(clip_rect, &Color::rgb8(25, 25, 25), 1.);
-        }
+        ctx.stroke(clip_rect, &Color::rgb8(25, 25, 25), 1.);
 
         self.inner.paint(ctx, data, env);
     }
