9a595542b7dd37d810701ea4ffe9f659bd8254df
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-03-31 03:21:22 +1300

    began implementation of App

--- src/core/app.rs
+++ src/core/app.rs
@@ -1,5 +1,27 @@
-use crate::core::cache::Cache;
+use std::sync::Arc;
 
-struct App {
-    cache: Cache,
+use druid::Data;
+use crate::core::{Cache, Graph};
+
+#[derive(Clone, Data)]
+pub struct App {
+    cache: Arc<Cache>,
+    graph: Graph,
+}
+
+impl App {
+    pub fn new() -> App {
+        App {
+            cache: Arc::new(Cache::new()),
+            graph: Graph::new(),
+        }
+    }
+
+    pub fn get_cache(&self) -> &Cache {
+        &self.cache
+    }
+
+    pub fn get_cache_mut(&mut self) -> Option<&mut Cache> {
+        Arc::get_mut(&mut self.cache)
+    }
 }
