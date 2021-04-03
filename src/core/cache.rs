3d83bc8065fe138c5a664e9c3c62a96bf72fd8ac
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-04-03 18:10:03 +1300

    made small fixes

--- src/core/cache.rs
+++ src/core/cache.rs
@@ -2,7 +2,7 @@ use std::{marker::PhantomData, mem::replace};
 
 use anymap::AnyMap;
 
-struct CacheIndex<T> {
+pub struct CacheIndex<T> {
     value_type: PhantomData<T>,
     index: usize,
 }
@@ -35,38 +35,44 @@ impl Cache {
         self.data.insert(Vec::<T>::new());
     }
 
-    pub fn insert<T: 'static>(&mut self, value: T) -> Option<CacheIndex<T>> {
+    pub fn insert<T: 'static>(&mut self, value: T) -> CacheIndex<T> {
         if !self.data.contains::<T>() {
             self.register::<T>();
         }
         let vec = self.data.get_mut::<Vec<T>>().unwrap();
         vec.push(value);
-        return Some(CacheIndex::<T>::new(vec.len() - 1));
+        CacheIndex::<T>::new(vec.len() - 1)
     }
 
     pub fn get<T: 'static>(&self, index: &CacheIndex<T>) -> Option<&T> {
         match self.data.get::<Vec<T>>() {
-            Some(vec) => {
-                vec.get(*index.index())
-            }
-            None => return None
+            Some(vec) => vec.get(*index.index()),
+            None => return None,
         }
     }
 
     pub fn get_mut<T: 'static>(&mut self, index: &CacheIndex<T>) -> Option<&mut T> {
         match self.data.get_mut::<Vec<T>>() {
-            Some(vec) => {
-                vec.get_mut(*index.index())
-            }
-            None => return None
+            Some(vec) => vec.get_mut(*index.index()),
+            None => return None,
         }
     }
 
     pub fn set<T: 'static>(&mut self, index: &CacheIndex<T>, new_value: T) {
-        replace(self.data.get_mut::<Vec<T>>().unwrap().get_mut(*index.index()).unwrap(), new_value);
+        replace(
+            self.data
+                .get_mut::<Vec<T>>()
+                .unwrap()
+                .get_mut(*index.index())
+                .unwrap(),
+            new_value,
+        );
     }
 
     pub fn remove<T: 'static>(&mut self, index: &CacheIndex<T>) -> T {
-        self.data.get_mut::<Vec<T>>().unwrap().remove(*index.index())
+        self.data
+            .get_mut::<Vec<T>>()
+            .unwrap()
+            .remove(*index.index())
     }
 }
