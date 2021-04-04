9565bf765f4994ff4aa9ff894af866ab96aa22cd
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-04-04 13:11:11 +1200

    created AllocatedVec

--- src/core/cache.rs
+++ src/core/cache.rs
@@ -1,4 +1,4 @@
-use std::{any::{Any, TypeId}, mem::replace};
+use std::any::TypeId;
 
 use anymap::AnyMap;
 
@@ -8,7 +8,7 @@ pub struct CacheIndex {
 }
 
 impl CacheIndex {
-    pub fn new<T: 'static>(index: usize) -> CacheIndex {
+    pub fn new<T: Default + 'static>(index: usize) -> CacheIndex {
         CacheIndex {
             type_id: TypeId::of::<T>(),
             index,
@@ -19,7 +19,7 @@ impl CacheIndex {
         &self.index
     }
 
-    pub fn is_type<T: 'static>(&self) -> bool {
+    pub fn is_type<T: Default + 'static>(&self) -> bool {
         self.type_id == TypeId::of::<T>()
     }
 }
@@ -35,48 +35,86 @@ impl Cache {
         }
     }
 
-    pub fn register<T: 'static>(&mut self) {
-        self.data.insert(Vec::<T>::new());
+    pub fn register<T: Default + 'static>(&mut self) {
+        self.data.insert(AllocatedVec::<T>::new());
     }
 
-    pub fn insert<T: 'static>(&mut self, value: T) -> CacheIndex {
+    pub fn insert<T: Default + 'static>(&mut self, value: T) -> CacheIndex {
         if !self.data.contains::<T>() {
             self.register::<T>();
         }
-        let vec = self.data.get_mut::<Vec<T>>().unwrap();
+        let vec = self.data.get_mut::<AllocatedVec<T>>().unwrap();
         vec.push(value);
         CacheIndex::new::<T>(vec.len() - 1)
     }
 
-    pub fn get<T: 'static>(&self, index: &CacheIndex) -> Option<&T> {
-        match self.data.get::<Vec<T>>() {
-            Some(vec) => vec.get(*index.index()),
+    pub fn get<T: Default + 'static>(&self, cache_index: &CacheIndex) -> Option<&T> {
+        match self.data.get::<AllocatedVec<T>>() {
+            Some(vec) => vec.get(*cache_index.index()),
             None => return None,
         }
     }
 
-    pub fn get_mut<T: 'static>(&mut self, index: &CacheIndex) -> Option<&mut T> {
-        match self.data.get_mut::<Vec<T>>() {
-            Some(vec) => vec.get_mut(*index.index()),
+    pub fn get_mut<T: Default + 'static>(&mut self, cache_index: &CacheIndex) -> Option<&mut T> {
+        match self.data.get_mut::<AllocatedVec<T>>() {
+            Some(vec) => vec.get_mut(*cache_index.index()),
             None => return None,
         }
     }
 
-    pub fn set<T: 'static>(&mut self, index: &CacheIndex, new_value: T) {
-        replace(
-            self.data
-                .get_mut::<Vec<T>>()
-                .unwrap()
-                .get_mut(*index.index())
-                .unwrap(),
-            new_value,
-        );
+    pub fn set<T: Default + 'static>(&mut self, cache_index: &CacheIndex, value: T) {
+        self.data.get_mut::<AllocatedVec<T>>().unwrap().set(*cache_index.index(), value)
     }
 
-    pub fn remove<T: 'static>(&mut self, index: &CacheIndex) -> T {
+    pub fn remove<T: Default + 'static>(&mut self, cache_index: &CacheIndex) {
         self.data
-            .get_mut::<Vec<T>>()
+            .get_mut::<AllocatedVec<T>>()
             .unwrap()
-            .remove(*index.index())
+            .remove(*cache_index.index());
+    }
+}
+
+pub struct AllocatedVec<T> {
+    vec: Vec<T>,
+    free: Vec<usize>,
+}
+
+impl<T: Default> AllocatedVec<T> {
+    pub fn new() -> AllocatedVec<T> {
+        AllocatedVec {
+            vec: Vec::new(),
+            free: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, value: T) {
+        match self.free.pop() {
+            Some(index) => self.set(index, value),
+            None => self.vec.push(value),
+        }
+    }
+
+    pub fn set(&mut self, index: usize, value: T) {
+        if index < self.vec.len() {
+            self.vec[index] = value;
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        self.vec.get(index)
+    }
+
+    pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+        self.vec.get_mut(index)
+    }
+
+    pub fn remove(&mut self, index: usize) {
+        self.set(index, T::default());
+        self.free.push(index);
+        self.free.sort_unstable_by(|a, b| b.cmp(a));
+    }
+
+    pub fn len(&self) -> usize {
+        self.vec.len()
     }
 }
