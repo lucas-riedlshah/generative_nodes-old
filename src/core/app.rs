e5950417be90c38a2e152538cc9e7c8bc0c8e6c9
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-04-10 15:32:05 +1200

    moved nodes into an AllocatedVec

--- src/core/app.rs
+++ src/core/app.rs
@@ -1,4 +1,4 @@
-use crate::core::{Cache, Node};
+use crate::core::{Cache, Node, AllocatedVec};
 
 /// Used to determine the compute order of nodes.
 enum EdgeType {
@@ -16,7 +16,7 @@ struct Edge {
 
 pub struct App {
     cache: Cache,
-    nodes: Vec<Node>,
+    nodes: AllocatedVec<Node>,
     // from_node, from_port, to_node, to_port
     edges: Vec<Edge>,
 }
@@ -25,24 +25,35 @@ impl App {
     pub fn new() -> App {
         App {
             cache: Cache::new(),
-            nodes: Vec::new(),
+            nodes: AllocatedVec::new(),
             edges: Vec::new(),
         }
     }
 
-    pub fn add_node(&mut self, node_factory: fn(cache: &mut Cache) -> Node) {
-        self.nodes.push((node_factory)(&mut self.cache));
+    pub fn add_node(&mut self, node_factory: fn(cache: &mut Cache) -> Node) -> usize {
+        self.nodes.push((node_factory)(&mut self.cache))
     }
 
     pub fn remove_node(&mut self, node_index: usize) {
-        let mut removed_node = self.nodes.remove(node_index);
-        // need to handle removing edges to the removed node.
+        let mut removed_node = self.nodes.remove(node_index).unwrap();
+
+        for index in (0..self.edges.len()).rev() {
+            let edge = self.edges.get(index).unwrap();
+            if edge.from_node == node_index || edge.to_node == node_index {
+                self.remove_edge(index);
+            }
+        }
+
         removed_node.remove_all_cache(&mut self.cache);
     }
 
+    pub fn get_node(&self, node_index: usize) -> &Node {
+        &self.nodes.get(node_index).unwrap()
+    }
+
     pub fn add_edge(&mut self, from_node: usize, from_port: usize, to_node: usize, to_port: usize) {
-        let new_cache_index = self.nodes[from_node].get_output(from_port).unwrap().clone();
-        self.nodes[to_node].connect_input(to_port, new_cache_index, &mut self.cache);
+        let new_cache_index = self.nodes.get(from_node).unwrap().get_output(from_port).unwrap().clone();
+        self.nodes.get_mut(to_node).unwrap().connect_input(to_port, new_cache_index, &mut self.cache);
 
         let edge_type = if self
             .edges
@@ -65,12 +76,12 @@ impl App {
         println!("new edge count: {}", self.edges.len());
     }
 
-    fn remove_edge(&mut self, index: usize, to_node: usize, to_port: usize) {
+    fn remove_edge(&mut self, index: usize) {
+        let edge = self.edges.remove(index);
         self.nodes
-            .get_mut(to_node)
+            .get_mut(edge.to_node)
             .unwrap()
-            .disconnect_input(to_port, &mut self.cache);
-        self.edges.remove(index);
+            .disconnect_input(edge.to_port, &mut self.cache);
         println!("new edge count: {}", self.edges.len());
     }
 
@@ -81,7 +92,7 @@ impl App {
             .position(|edge| edge.to_node == to_node && edge.to_port == to_port);
 
         if let Some(index) = edge_index {
-            self.remove_edge(index, to_node, to_port)
+            self.remove_edge(index)
         }
     }
 
@@ -89,13 +100,18 @@ impl App {
         for index in (0..self.edges.len()).rev() {
             let edge = self.edges.get(index).unwrap();
             if edge.from_node == from_node && edge.from_port == from_port {
-                self.remove_edge(index, edge.to_node, edge.to_port);
+                self.remove_edge(index);
             }
         }
     }
 
+    pub fn get_cache_mut(&mut self) -> &mut Cache {
+        &mut self.cache
+    }
+
     pub fn compute(&mut self) {
-        for node in self.nodes.iter() {
+        for i in 0..self.nodes.len() {
+            let node = self.nodes.get(i).unwrap();
             node.compute(&mut self.cache)
         }
     }
