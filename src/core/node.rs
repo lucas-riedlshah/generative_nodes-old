9f499d37b17bc30966db6bcc82dee554051b611e
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-04-12 20:27:26 +1200

    continued to refine how Node is implemented.

--- src/core/node.rs
+++ src/core/node.rs
@@ -14,11 +14,11 @@ pub struct Node {
     compute: fn(inputs: &Vec<CacheIndex>, outputs: &Vec<CacheIndex>, cache: &mut Cache),
     /// Called when the node is about to be removed. Primary purpose is to remove any input/output values from the [Cache].
     remove_all_cache: fn(inputs: &Vec<CacheIndex>, outputs: &Vec<CacheIndex>, cache: &mut Cache),
-    create_input_cache: fn(port: usize, cache: &mut Cache) -> Option<CacheIndex>,
-    remove_input_cache: fn(port: usize, cache_index: CacheIndex, cache: &mut Cache),
     // TODO: consider replacing the create/remove input cache funcs with complete disconnect/connect funcs.
     /// Called when an input port is disconnected and so a new value must be created in the Cache.
+    create_input_cache: fn(node: &Node, port: usize, cache: &mut Cache) -> Option<CacheIndex>,
     /// Called when an input port is connected and so the current ("internal") value must be removed from the [Cache].
+    remove_input_cache: fn(node: &Node, port: usize, cache: &mut Cache),
 }
 
 impl Node {
@@ -51,8 +51,8 @@ impl Node {
 
     pub fn with_create_remove_input_cache(
         mut self,
-        create_input_cache_func: fn(port: usize, cache: &mut Cache) -> Option<CacheIndex>,
-        remove_input_cache_func: fn(port: usize, cache_index: CacheIndex, cache: &mut Cache),
+        create_input_cache_func: fn(node: &Node, port: usize, cache: &mut Cache) -> Option<CacheIndex>,
+        remove_input_cache_func: fn(node: &Node, port: usize, cache: &mut Cache),
     ) -> Self {
         self.create_input_cache = create_input_cache_func;
         self.remove_input_cache = remove_input_cache_func;
@@ -60,31 +60,41 @@ impl Node {
     }
 
     pub fn connect_input(&mut self, port: usize, new_cache_index: CacheIndex, cache: &mut Cache) {
-        // investigate replacing these functions with something that uses TypeId instead of "dynamic" functions
-        let old_cache_index = self.inputs.remove(port);
-        (self.remove_input_cache)(port, old_cache_index, cache);
-        self.inputs.insert(port, new_cache_index);
+        // TODO: investigate replacing these functions with something that uses TypeId & the raw anymap instead of "dynamic" functions
+        (self.remove_input_cache)(&self, port, cache);
+        *self.inputs.get_mut(port).unwrap() = new_cache_index;
     }
 
     pub fn disconnect_input(&mut self, port: usize, cache: &mut Cache) {
         self.inputs.remove(port);
         self.inputs
-            .insert(port, (self.create_input_cache)(port, cache).unwrap());
+            .insert(port, (self.create_input_cache)(&self, port, cache).unwrap());
     }
 
     pub fn get_output(&self, port: usize) -> Option<&CacheIndex> {
         self.outputs.get(port)
     }
 
+    pub fn get_inputs(&self) -> &Vec<CacheIndex> {
+        &self.inputs
+    }
+
+    pub fn get_outputs(&self) -> &Vec<CacheIndex> {
+        &self.outputs
+    }
+
     pub fn compute(&self, cache: &mut Cache) {
         (self.compute)(&self.inputs, &self.outputs, cache);
     }
 
-    pub fn remove_all_cache(&mut self, cache: &mut Cache) {}
+    pub fn remove_all_cache(&mut self, cache: &mut Cache) {
+        (self.remove_all_cache)(&self.inputs, &self.outputs, cache);
+    }
 }
 
+// TODO: switch to Option<fn> instead of these default fn's. 
 fn default_compute(inputs: &Vec<CacheIndex>, outputs: &Vec<CacheIndex>, cache: &mut Cache) {}
-fn default_remove_input_cache(port: usize, new_cache_index: CacheIndex, cache: &mut Cache) {}
-fn default_create_input_cache(port: usize, cache: &mut Cache) -> Option<CacheIndex> {
+fn default_remove_input_cache(node: &Node, port: usize, cache: &mut Cache) {}
+fn default_create_input_cache(node: &Node, port: usize, cache: &mut Cache) -> Option<CacheIndex> {
     None
 }
