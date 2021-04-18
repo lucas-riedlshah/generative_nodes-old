a4dbcd43614d08514e6953a751e89265b112eafa
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-04-18 19:05:42 +1200

    added widgets

--- src/nodes/value_node.rs
+++ src/nodes/value_node.rs
@@ -1,7 +1,17 @@
-use crate::core::{Cache, CacheIndex, Node};
+use std::{cell::RefCell, rc::Rc};
 
-pub fn value_node_factory(cache: &mut Cache) -> Node {
-    let value = cache.insert(1.);
+use druid::{
+    widget::{Container, CrossAxisAlignment, Flex, Label},
+    Color, Widget, WidgetExt,
+};
+
+use crate::{
+    core::{App, Cache, CacheIndex, Node},
+    gui::{graph_widget::PortDirection, node_widget::NodeWidget, port_widget::PortWidget},
+};
+
+pub fn node_factory(cache: &mut Cache) -> Node {
+    let value = cache.insert(10.);
 
     let inputs = Vec::new();
 
@@ -13,4 +23,32 @@ pub fn value_node_factory(cache: &mut Cache) -> Node {
 
 fn remove_all_cache(inputs: &Vec<CacheIndex>, outputs: &Vec<CacheIndex>, cache: &mut Cache) {
     cache.remove::<f64>(&outputs[0]);
+}
+
+pub fn widget_factory(index: usize) -> Box<dyn Widget<Rc<RefCell<App>>>> {
+    Box::new(NodeWidget::new(
+        Container::new(
+            Flex::column()
+                .with_child(Label::new("Value"))
+                .with_spacer(5.)
+                .with_child(
+                    // Outputs
+                    Flex::column()
+                        .cross_axis_alignment(CrossAxisAlignment::End)
+                        .with_child(
+                            Flex::row()
+                                .with_child(Label::new("Value"))
+                                .with_spacer(5.)
+                                .with_child(PortWidget::new(index, 0, PortDirection::Output)),
+                        )
+                        .with_spacer(5.)
+                        .expand_width(),
+                )
+                .fix_width(150.)
+                .padding(5.),
+        )
+        .rounded(10.)
+        .background(Color::rgba8(50, 50, 50, 230))
+        .border(Color::rgb8(25, 25, 25), 1.),
+    ))
 }
--- src/nodes/vector_node.rs
+++ src/nodes/vector_node.rs
@@ -1,8 +1,17 @@
+use std::{cell::RefCell, rc::Rc};
+
+use druid::{
+    widget::{Container, CrossAxisAlignment, Flex, Label},
+    Color, Widget, WidgetExt,
+};
 use nalgebra::Vector2;
 
-use crate::core::{Cache, CacheIndex, Node};
+use crate::{
+    core::{App, Cache, CacheIndex, Node},
+    gui::{graph_widget::PortDirection, node_widget::NodeWidget, port_widget::PortWidget},
+};
 
-pub fn vector_node_factory(cache: &mut Cache) -> Node {
+pub fn node_factory(cache: &mut Cache) -> Node {
     let x = cache.insert(0.);
     let y = cache.insert(0.);
     let vector = cache.insert(Vector2::new(0., 0.));
@@ -45,4 +54,54 @@ fn remove_all_cache(inputs: &Vec<CacheIndex>, outputs: &Vec<CacheIndex>, cache: 
     cache.remove::<f64>(&inputs[0]);
     cache.remove::<f64>(&inputs[1]);
     cache.remove::<Vector2<f64>>(&outputs[0]);
+}
+
+pub fn widget_factory(index: usize) -> Box<dyn Widget<Rc<RefCell<App>>>> {
+    Box::new(NodeWidget::new(
+        Container::new(
+            Flex::column()
+                .with_child(Label::new("Vector2"))
+                .with_spacer(5.)
+                .with_child(
+                    // Inputs
+                    Flex::column()
+                        .cross_axis_alignment(CrossAxisAlignment::Start)
+                        .with_child(
+                            Flex::row()
+                                .with_child(PortWidget::new(index, 0, PortDirection::Input))
+                                .with_spacer(5.)
+                                // .with_child(TextBox::new().lens(StringInputLens("string"))),
+                                .with_child(Label::new("X")),
+                        )
+                        .with_spacer(5.)
+                        .with_child(
+                            Flex::row()
+                                .with_child(PortWidget::new(index, 1, PortDirection::Input))
+                                .with_spacer(5.)
+                                // .with_child(TextBox::new().lens(StringInputLens("string"))),
+                                .with_child(Label::new("Y")),
+                        )
+                        .expand_width(),
+                )
+                .with_spacer(5.)
+                .with_child(
+                    // Outputs
+                    Flex::column()
+                        .cross_axis_alignment(CrossAxisAlignment::End)
+                        .with_child(
+                            Flex::row()
+                                .with_child(Label::new("Vector2"))
+                                .with_spacer(5.)
+                                .with_child(PortWidget::new(index, 0, PortDirection::Output)),
+                        )
+                        .with_spacer(5.)
+                        .expand_width(),
+                )
+                .fix_width(150.)
+                .padding(5.),
+        )
+        .rounded(10.)
+        .background(Color::rgba8(50, 50, 50, 230))
+        .border(Color::rgb8(25, 25, 25), 1.),
+    ))
 }
