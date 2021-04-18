e7cdd6c0a8087ae5380a231e41b3c0bd1944ff71
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-04-18 19:06:12 +1200

    massively simplified

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,22 @@
 mod core;
-// mod gui;
+mod gui;
 mod nodes;
 
+use std::{cell::RefCell, rc::Rc};
+
+use druid::{AppLauncher, PlatformError, WindowDesc};
+
 use crate::core::App;
-// use crate::gui::graph_widget::GraphWidget;
-use crate::nodes::{particle_node_factory, value_node_factory, vector_node_factory};
+use crate::gui::delegate::Delegate;
+use crate::gui::graph_widget::GraphWidget;
 
-fn main() {
-    let mut app = App::new();
+fn main() -> Result<(), PlatformError> {
+    let app = App::new().with_factories(nodes::node_factories());
 
-    let value_node = app.add_node(value_node_factory);
-    let vector_node = app.add_node(vector_node_factory);
-    let particle_node = app.add_node(particle_node_factory);
-    
-    let value_cache_index = app.get_node(value_node).get_output(0).unwrap().clone();
-    *app.get_cache_mut().get_mut(&value_cache_index).unwrap() = 100.;
+    let main_window = WindowDesc::new(GraphWidget::new());
 
-    app.add_edge(value_node, 0, vector_node, 0);
-    app.add_edge(value_node, 0, vector_node, 1);
-    app.add_edge(vector_node, 0, particle_node, 2);
-
-    for i in 0..100  {
-        app.compute();
-    }
-
-    // let main_window = WindowDesc::new(GraphWidget::new());
-    // AppLauncher::with_window(main_window)
-    //     // .log_to_console()
-    //     .launch(data)
+    AppLauncher::with_window(main_window)
+        .delegate(Delegate::new(nodes::node_widget_factories()))
+        // .log_to_console()
+        .launch(Rc::new(RefCell::new(app)))
 }
