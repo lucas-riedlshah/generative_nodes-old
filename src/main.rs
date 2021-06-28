c4f2175aca82ef6d0c2a788345407fc8f2ce0175
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-06-28 20:24:08 +1200

    testing stuff for clipbox

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ mod nodes;
 
 use std::{cell::RefCell, rc::Rc};
 
-use druid::{widget::Split, AppLauncher, PlatformError, WindowDesc};
+use druid::Point;
+use druid::{widget::{Split, ClipBox, }, AppLauncher, PlatformError, WindowDesc};
 use gui::viewer_2d::Viewer2D;
 
 use crate::core::App;
 use crate::gui::delegate::Delegate;
-use crate::gui::graph_widget::GraphWidget;
+use crate::gui::graph_widget::Graph;
 
 // TODO: Need to go through all files and check where I use vec[i] vs. vec.get(i) and make sure it all makes sense and stuff.
 
 fn main() -> Result<(), PlatformError> {
     let app = App::new().with_factories(nodes::node_factories());
 
-    let main_window = WindowDesc::new(Split::columns(GraphWidget::new(), Viewer2D::new()).draggable(true));
+    let mut clip = ClipBox::new(Graph::new());
+    clip.pan_to(Point::new(100., -50.));
+    
+    let main_window = WindowDesc::new(Split::columns(clip, Viewer2D::new()).draggable(true));
 
     AppLauncher::with_window(main_window)
         .delegate(Delegate::new(nodes::node_widget_factories()))
